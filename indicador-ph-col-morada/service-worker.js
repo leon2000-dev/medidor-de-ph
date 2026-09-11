acdfd9689b6ab232d61acc638fd1c960c0a2a710
leon2000-dev <leon06a2011@gmail.com>
2026-09-10 19:41:44 -0600

    Delete indicador-ph-col-morada directory

--- indicador-ph-col-morada/service-worker.js
+++
@@ -1,9 +0,0 @@
-const CACHE = 'color-ph-v1';
-const FILES = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
-
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request)));
-});
